c3cef51b2473f0e337dedc7a5f202e845f057dfd
Aziz TAS <tasaziz4106@gmail.com>
2026-01-29 23:42:54 +0100

    Add debug exception handler to see stack traces

--- src/main/java/com/hoaxify/ws/error/GlobalErrorHandler.java
+++ src/main/java/com/hoaxify/ws/error/GlobalErrorHandler.java
@@ -1,5 +1,6 @@
 package com.hoaxify.ws.error;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.DisabledException;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 @ControllerAdvice
 public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
@@ -24,5 +27,19 @@ public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
         }
         return ResponseEntity.status(error.getStatus()).body(error);
     }
-    
+
+    @ExceptionHandler(Exception.class)
+    ResponseEntity<?> handleAllExceptions(Exception exception, HttpServletRequest request) {
+        ApiError error = new ApiError();
+        error.setPath(request.getRequestURI());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        
+        StringWriter sw = new StringWriter();
+        exception.printStackTrace(new PrintWriter(sw));
+        String stackTrace = sw.toString();
+        
+        error.setMessage(exception.getClass().getName() + ": " + exception.getMessage() + " --- " + stackTrace.substring(0, Math.min(500, stackTrace.length())));
+        
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
 }
