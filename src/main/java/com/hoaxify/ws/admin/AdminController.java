3d02a817f2d1845a3cd80b00a2c9d09aa9fc8335
Aziz TAS <tasaziz4106@gmail.com>
2026-01-30 02:52:00 +0100

    Add @Transactional to deleteUserById

--- src/main/java/com/hoaxify/ws/admin/AdminController.java
+++ src/main/java/com/hoaxify/ws/admin/AdminController.java
@@ -3,6 +3,7 @@ package com.hoaxify.ws.admin;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -48,6 +49,7 @@ public class AdminController {
         return orderRepository.count();
     }
 
+    @Transactional
     @DeleteMapping("/users/{id}")
     String deleteUserById(@PathVariable Long id) {
         cartRepository.deleteByUserId(id);
