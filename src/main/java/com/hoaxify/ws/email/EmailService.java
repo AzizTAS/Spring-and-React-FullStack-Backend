d5591d43b329c560cc9394e67dc90bd3da355b39
Aziz TAS <tasaziz4106@gmail.com>
2026-01-30 01:10:40 +0100

    Add detailed logging and timeout settings to EmailService

--- src/main/java/com/hoaxify/ws/email/EmailService.java
+++ src/main/java/com/hoaxify/ws/email/EmailService.java
@@ -2,6 +2,8 @@ package com.hoaxify.ws.email;
 
 import java.util.Properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -18,6 +20,8 @@ import jakarta.mail.internet.MimeMessage;
 @Service
 public class EmailService {
 
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
+
     JavaMailSenderImpl mailSender;
 
     private final HoaxifyProperties hoaxifyProperties;
@@ -31,6 +35,7 @@ public class EmailService {
 
     @PostConstruct
     public void initialize() {
+        logger.info("Initializing EmailService with host: {}", hoaxifyProperties.getEmail().host());
         this.mailSender = new JavaMailSenderImpl();
         mailSender.setHost(hoaxifyProperties.getEmail().host());
         mailSender.setPort(hoaxifyProperties.getEmail().port());
@@ -42,7 +47,12 @@ public class EmailService {
         properties.put("mail.smtp.starttls.enable", "true");
         properties.put("mail.smtp.starttls.required", "true");
         properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
-
+        properties.put("mail.smtp.connectiontimeout", "10000"); // 10 seconds
+        properties.put("mail.smtp.timeout", "10000"); // 10 seconds
+        properties.put("mail.smtp.writetimeout", "10000"); // 10 seconds
+        properties.put("mail.debug", "true"); // Debug logging
+        
+        logger.info("EmailService initialized successfully");
     }
 
     String activationEmail = """
@@ -56,47 +66,59 @@ public class EmailService {
 
     @Async
     public void sendActivationEmail(String email, String activationToken) {
-        var activationUrl = hoaxifyProperties.getClient().host() + "/activation/" + activationToken;
-        var title = messageSource.getMessage("hoaxify.mail.user.created.title", null, LocaleContextHolder.getLocale());
-        var clickHere = messageSource.getMessage("hoaxify.mail.click.here", null, LocaleContextHolder.getLocale());
-
-        var mailBody = activationEmail
-                .replace("${url}", activationUrl)
-                .replace("${title}", title)
-                .replace("${clickHere}", clickHere);
-
-        MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "UTF-8");
+        logger.info("Attempting to send activation email to: {}", email);
         try {
+            var activationUrl = hoaxifyProperties.getClient().host() + "/activation/" + activationToken;
+            var title = messageSource.getMessage("hoaxify.mail.user.created.title", null, LocaleContextHolder.getLocale());
+            var clickHere = messageSource.getMessage("hoaxify.mail.click.here", null, LocaleContextHolder.getLocale());
+
+            var mailBody = activationEmail
+                    .replace("${url}", activationUrl)
+                    .replace("${title}", title)
+                    .replace("${clickHere}", clickHere);
+
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "UTF-8");
             message.setFrom(hoaxifyProperties.getEmail().from());
             message.setTo(email);
             message.setSubject(title);
             message.setText(mailBody, true);
+            
+            logger.info("Sending activation email from: {} to: {}", hoaxifyProperties.getEmail().from(), email);
+            this.mailSender.send(mimeMessage);
+            logger.info("Activation email sent successfully to: {}", email);
         } catch (MessagingException e) {
-            e.printStackTrace();
+            logger.error("MessagingException while sending activation email to {}: {}", email, e.getMessage(), e);
+        } catch (Exception e) {
+            logger.error("Unexpected error while sending activation email to {}: {}", email, e.getMessage(), e);
         }
-
-        this.mailSender.send(mimeMessage);
     }
 
     @Async
     public void sendPasswordResetEmail(String email, String passwordResetToken) {
-        String passwordResetUrl = hoaxifyProperties.getClient().host() + "/password-reset/set?tk=" + passwordResetToken;
-        MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "UTF-8");
-        var title = "Reset your password";
-        var clickHere = messageSource.getMessage("hoaxify.mail.click.here", null, LocaleContextHolder.getLocale());
-        var mailBody = activationEmail.replace("${url}", passwordResetUrl).replace("${title}", title)
-                .replace("${clickHere}", clickHere);
+        logger.info("Attempting to send password reset email to: {}", email);
         try {
+            String passwordResetUrl = hoaxifyProperties.getClient().host() + "/password-reset/set?tk=" + passwordResetToken;
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "UTF-8");
+            var title = "Reset your password";
+            var clickHere = messageSource.getMessage("hoaxify.mail.click.here", null, LocaleContextHolder.getLocale());
+            var mailBody = activationEmail.replace("${url}", passwordResetUrl).replace("${title}", title)
+                    .replace("${clickHere}", clickHere);
+            
             message.setFrom(hoaxifyProperties.getEmail().from());
             message.setTo(email);
             message.setSubject(title);
             message.setText(mailBody, true);
+            
+            logger.info("Sending password reset email to: {}", email);
+            this.mailSender.send(mimeMessage);
+            logger.info("Password reset email sent successfully to: {}", email);
         } catch (MessagingException e) {
-            e.printStackTrace();
+            logger.error("MessagingException while sending password reset email to {}: {}", email, e.getMessage(), e);
+        } catch (Exception e) {
+            logger.error("Unexpected error while sending password reset email to {}: {}", email, e.getMessage(), e);
         }
-        this.mailSender.send(mimeMessage);
     }
 
 }
